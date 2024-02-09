a807fe6390a664964b92d33372dd3362f5f2fd7e
Paul K <spkim0921@gmail.com>
2024-02-09 11:42:56 -0800

    5th commit
    debug deployment

--- app.ts
+++ app.ts
@@ -28,7 +28,7 @@ app.use(cors());
 const server = http.createServer(app);
 const io = new SocketServer(server, {
   cors: {
-    origin: "http://localhost:5173",
+    origin: "https://codingcapy.github.io/capytalk",
   },
 });
 io.on("connection", (socket) => {
