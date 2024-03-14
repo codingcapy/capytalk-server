792e2fed29136e662590042917ecd4f9de9672aa
Paul K <spkim0921@gmail.com>
2024-03-14 08:23:22 -0700

    14th commit
    implementing error handling

--- models/Message.ts
+++ models/Message.ts
@@ -9,7 +9,7 @@ description: message model schema for CapyTalk API server
 import mongoose from "mongoose";
 
 export const MessageSchema = new mongoose.Schema({
-    content: { type: String, required: true, default: "New Chat" },
+    content: { type: String, required: true, default: "New Chat", maxlength: [25000, 'username char limit is 25000'] },
     date: { type: Date, required: true, default: Date.now },
     username: { type: String, required: [true, 'username is required'] },
     chatId: { type: Number, required: [true, 'chatId is required'] },
